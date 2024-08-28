161e012f1800a39c9eb9d08529abdf3f4bf4a807
Girwar-Sahu <girvar786sahu@gmail.com>
2024-08-28 13:59:08 +0530

    create signup logic

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,8 +1,54 @@
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { generateJwtToken } from "../utils/generateToken.js";
 
 export const signUp = async (req, res) => {
-  res.json("signup route");
+  const { email, password, name } = req.body;
+  try {
+    if (!email || !password || !name) {
+      throw new Error("all fields are required");
+    }
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res
+        .status(400)
+        .json({ success: false, message: "user already exist" });
+    }
+    const hashPassword = await bcryptjs.hash(password, 10);
+    const varificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+
+    const user = new User({
+      email,
+      password: hashPassword,
+      name,
+      varificationToken,
+      varificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, //24 hours
+    });
+    
+    await user.save();
+    const token = generateJwtToken(res, user._id); // generate JWT token
+    res
+      .status(201)
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
+      .json({
+        success: true,
+        message: "user is created",
+        use: {
+          ...user._doc,
+          password: undefined,
+        },
+      });
+
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 export const signIn = async (req, res) => {
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import { signIn, signOut, signUp } from "../controller/auth.controller.js";
 const router = express.Router();
 
-router.get("/signup", signUp);
+router.post("/signup", signUp);
 router.get("/sigin", signIn);
 router.get("/signout", signOut);
 
