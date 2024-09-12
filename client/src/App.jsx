8d74c3502d566f4b8643bba27f286009b28d7fde
Girwar-Sahu <girvar786sahu@gmail.com>
2024-09-12 12:55:27 +0530

    create the signup page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import FloatingShape from "./components/FloatingShape";
+import SigninPage from "./pages/SigninPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
-    <div className='text-xl text-green-700'>App is working...</div>
-  )
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex justify-center items-center relative overflow-hidden">
+      <FloatingShape
+        color="bg-green-500"
+        size="w-64 h-64"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-emerald-500"
+        size="w-48 h-48"
+        top="70%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-lime-500"
+        size="w-32 h-32"
+        top="40%"
+        left="-10%"
+        delay={2}
+      />
+      <Routes>
+        <Route path="/" element={"Home"} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signin" element={<SigninPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
